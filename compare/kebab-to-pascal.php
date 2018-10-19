2a462b15578e97a4453f8d35e0063b1a776d4d20
Alain D'Ettorre <alain.det@gmail.com>
2018-10-19 20:37:57 +0200

    Add two tests to kebab-to-pascal

--- compare/kebab-to-pascal.php
+++ compare/kebab-to-pascal.php
@@ -2,19 +2,35 @@
 
 // KEBAB CASE TO PASCAL CASE
 // ----------------------------------------------------------------------------
-// This test compares two functions to translate PascalCase strings to
-// kebab-case strings. The first function uses a regex approach via
-// preg_replace_callback(), while the second function uses array splitting and
-// gluing via explode() and array_reduce(). Which one's faster?
-
+// This test compares several functions to translate PascalCase strings to
+// kebab-case strings. Which one's faster?
 
 // Data -----------------------------------------------------------------------
 $iterations = 1000 * 100;
 $tests = [];
 $lorem = 'morbi-congue-tellus-ut-ultricies-congue-suspendisse-potenti-integer-tellus-quam-varius-a-elementum-eu-mattis-vel-ex-aenean-lacus-tellus-hendrerit-vel-ligula-feugiat-gravida-suscipit-magna-vestibulum-purus-nisi-imperdiet-eu-vehicula-at-feugiat-tempus-enim-in-hac-habitasse-platea-dictumst-cras-dapibus-lorem-id-dolor-mollis-sed-facilisis-massa-aliquet-etiam-pellentesque-arcu-nibh-quis-ullamcorper-quam-hendrerit-non-quisque-condimentum-mauris-ac-tortor-elementum-a-mattis-justo-gravida-sed-sollicitudin-condimentum-orci-non-dapibus';
 
+// Test ucwords() -------------------------------------------------------------
+$test = [
+    'label' => 'ucwords()',
+    'time' => microtime(true)
+];
 
-// Regex function -------------------------------------------------------------
+function ucwordsKebabToPascal(string $kebab): string
+{
+    return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $kebab)));
+}
+
+for ($i = 0; $i < $iterations; $i++) ucwordsKebabToPascal($lorem);
+$test['time'] = microtime(true) - $test['time'];
+$tests[] = $test;
+
+// Test preg_replace_callback() -----------------------------------------------
+$test = [
+    'label' => 'preg_replace_callback()',
+    'time' => microtime(true)
+];
+
 function regexKebabToPascal(string $kebab): string
 {
     return preg_replace_callback(
@@ -24,24 +40,7 @@ function regexKebabToPascal(string $kebab): string
     );
 }
 
-
-// Array split function -------------------------------------------------------
-function reduceKebabToPascal(string $kebab): string
-{
-    return array_reduce(explode('-',$kebab), function ($result, $word) {
-        return $result .= ucfirst($word);
-    } , '');    
-}
-
-
-// Test preg_replace_callback() -----------------------------------------------
-$test = [
-    'label' => 'preg_replace_callback()',
-    'time' => microtime(true)
-];
-for ($i = 0; $i < $iterations; $i++) {
-    regexKebabToPascal($lorem);
-}
+for ($i = 0; $i < $iterations; $i++) regexKebabToPascal($lorem);
 $test['time'] = microtime(true) - $test['time'];
 $tests[] = $test;
 
@@ -51,9 +50,40 @@ $test = [
     'label' => 'explode()+array_reduce()',
     'time' => microtime(true)
 ];
-for ($i = 0; $i < $iterations; $i++) {
-    reduceKebabToPascal($lorem);
+
+function reduceKebabToPascal(string $kebab): string
+{
+    return array_reduce(explode('-',$kebab), function ($result, $word) {
+        return $result .= ucfirst($word);
+    } , '');
 }
+
+for ($i = 0; $i < $iterations; $i++) reduceKebabToPascal($lorem);
+$test['time'] = microtime(true) - $test['time'];
+$tests[] = $test;
+
+// Test for loop --------------------------------------------------------------
+$test = [
+    'label' => 'for loop on letters',
+    'time' => microtime(true)
+];
+
+function forLoopKebabToPascal(string $kebab): string
+{
+    $uppercase = ['a'=>'A','b'=>'B','c'=>'C','d'=>'D','e'=>'E','f'=>'F','g'=>'G','h'=>'H','i'=>'I','j'=>'J','k'=>'K','l'=>'L','m'=>'M','n'=>'N','o'=>'O','p'=>'P','q'=>'Q','e'=>'E','s'=>'S','t'=>'T','u'=>'U','v'=>'V','w'=>'W','x'=>'X','y'=>'Y','z'=>'Z'];
+    $result = $uppercase[$kebab[0]];
+    for ($i = 1, $len = strlen($kebab); $i < $len; $i++) {
+        if ($kebab[$i] === '-') {
+            $i++;
+            $result .= $uppercase[$kebab[$i]];
+        } else {
+            $result .= $kebab[$i];
+        }
+    }
+    return $result;
+}
+
+for ($i = 0; $i < $iterations; $i++) reduceKebabToPascal($lorem);
 $test['time'] = microtime(true) - $test['time'];
 $tests[] = $test;
 
@@ -87,21 +117,34 @@ echo array_reduce(
 // Kebab case to Pascal case test
 // Iterations: 100000
 // ==============================
-// preg_replace_callback()  : 3509 ms : 114%
-// explode()+array_reduce() : 3089 ms : 100%
+// ucwords()                : 1183 ms : 100%
+// preg_replace_callback()  : 4182 ms : 353%
+// explode()+array_reduce() : 2612 ms : 221%
+// for loop on letters      : 2566 ms : 217%
 
 // #2
 // Kebab case to Pascal case test
 // Iterations: 100000
 // ==============================
-// preg_replace_callback()  : 3955 ms : 110%
-// explode()+array_reduce() : 3606 ms : 100%
+// ucwords()                : 1173 ms : 100%
+// preg_replace_callback()  : 3716 ms : 317%
+// explode()+array_reduce() : 2782 ms : 237%
+// for loop on letters      : 3116 ms : 266%
 
 // #3
 // Kebab case to Pascal case test
 // Iterations: 100000
 // ==============================
-// preg_replace_callback()  : 4165 ms : 130%
-// explode()+array_reduce() : 3194 ms : 100%
+// ucwords()                : 1338 ms : 100%
+// preg_replace_callback()  : 3662 ms : 274%
+// explode()+array_reduce() : 2903 ms : 217%
+// for loop on letters      : 2518 ms : 188%
 
-// Average: explode()+array_reduce() is 18% faster than preg_replace_callback()
+// #4
+// Kebab case to Pascal case test
+// Iterations: 100000
+// ==============================
+// ucwords()                : 1119 ms : 100%
+// preg_replace_callback()  : 3618 ms : 323%
+// explode()+array_reduce() : 2551 ms : 228%
+// for loop on letters      : 2478 ms : 221%
